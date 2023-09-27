9bab77188eb8cc271f9a97fc1f6f7a39e2834004
bryosabian <sabianbryo@gmail.com>
2023-09-27 13:38:38 +0300

    Lab Phase : Warehouse Module : Added dark and custom theme support for receive stock screen;

--- src/main/java/com/sabiantools/extensions/SabianView.kt
+++ src/main/java/com/sabiantools/extensions/SabianView.kt
@@ -1,11 +1,15 @@
 package com.sabiantools.extensions
 
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.LayerDrawable
 import android.os.Build
 import android.transition.Fade
 import android.transition.Transition
 import android.transition.TransitionManager
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.ColorInt
+import androidx.annotation.IdRes
 
 fun View.fadeVisibility(visibility: Int, duration: Long = 300) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -15,4 +19,16 @@ fun View.fadeVisibility(visibility: Int, duration: Long = 300) {
         TransitionManager.beginDelayedTransition(this.parent as ViewGroup, transition)
     }
     this.visibility = visibility
+}
+
+fun View.setLayerBackgroundColor(@IdRes layerItemID: Int, @ColorInt color: Int) {
+    try {
+        val layerDrawable = background.mutate() as? LayerDrawable ?: return
+        val bgDrawableItem = layerDrawable.findDrawableByLayerId(layerItemID) as? GradientDrawable
+                ?: return
+        bgDrawableItem.setColor(color)
+        background = layerDrawable
+    } catch (e: Throwable) {
+        e.printStackTrace()
+    }
 }
