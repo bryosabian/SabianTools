1f875fe2f8e8e0c4e037c01fc2ea4f65be2e0b2a
bryosabian <sabianbryo@gmail.com>
2023-10-04 16:33:37 +0300

    Lab Phase : Sales Module : Added dark and custom theme support for main dashboard screen

--- src/main/java/com/sabiantools/extensions/SabianViewGroup.kt
+++ src/main/java/com/sabiantools/extensions/SabianViewGroup.kt
@@ -3,12 +3,16 @@ package com.sabiantools.extensions
 import android.view.View
 import android.view.ViewGroup
 
-val ViewGroup.allChildren: List<View>
+val ViewGroup.allChildViews: List<View>
     get() {
         val all = arrayListOf<View>()
         val childCount: Int = childCount
         for (i in 0 until childCount) {
-            all.add(getChildAt(i));
+            val v = getChildAt(i)
+            all.add(v)
+            if (v is ViewGroup) {
+                all.addAll(v.allChildViews)
+            }
         }
         return all
     }
