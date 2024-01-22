25646d494c92e6876c85b29175c0ff00a5a29c6f
bryosabian <sabianbryo@gmail.com>
2024-01-22 17:47:57 +0300

    Lab Phase : Theme : Survey Questions : Added custom and dark theme support for all survey questions

--- src/main/java/com/sabiantools/modals/SabianEditTextModal.kt
+++ src/main/java/com/sabiantools/modals/SabianEditTextModal.kt
@@ -1,33 +1,28 @@
 package com.sabiantools.modals
 
-import android.app.Dialog
 import android.content.Context
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.ViewGroup
 import android.view.Window
-import android.view.WindowManager
-import android.view.animation.AnimationUtils
 import android.widget.TextView
+import androidx.annotation.ColorInt
 import com.gc.materialdesign.views.ButtonFlat
 import com.sabiantools.R
 import com.sabiantools.controls.texts.SabianCondensedEditText
-import kotlinx.android.synthetic.main.layout_modal_edit_text.*
+import kotlinx.android.synthetic.main.layout_modal_edit_text.btn_ModalEditTextCancel
+import kotlinx.android.synthetic.main.layout_modal_edit_text.btn_ModalEditTextOk
+import kotlinx.android.synthetic.main.layout_modal_edit_text.rll_ModalEditTextContainer
+import kotlinx.android.synthetic.main.layout_modal_edit_text.sct_ModalEditText
+import kotlinx.android.synthetic.main.layout_modal_edit_text.sct_ModalEditTextTitle
 
 class SabianEditTextModal(
-    context: Context,
-    private var onSave: (String?, SabianEditTextModal) -> Unit
+        context: Context,
+        private var onSave: (String?, SabianEditTextModal) -> Unit
 ) :
-    Dialog(context, R.style.SabianMaterialDialog) {
+        SabianCustomModal(context, R.style.SabianMaterialDialog) {
     private var txtTitle: TextView? = null
     private var sctEditText: SabianCondensedEditText? = null
     private var btnSubmit: ButtonFlat? = null
     private var btnCancel: ButtonFlat? = null
-    private var vgBodyContainer: ViewGroup? = null
-    private var animate = true
 
 
     var onCancel: ((String?, SabianEditTextModal) -> Unit)? = null
@@ -65,15 +60,44 @@ class SabianEditTextModal(
             field = value
         }
 
+    @ColorInt
+    var okayButtonColor: Int? = null
+        set(value) {
+            if (value != null)
+                btnSubmit?.let { it.setBackgroundColor(value) }
+            field = value
+        }
+
+    @ColorInt
+    var cancelButtonColor: Int? = null
+        set(value) {
+            if (value != null)
+                btnCancel?.let { it.setBackgroundColor(value) }
+            field = value
+        }
+
+    var buttonColors: Int? = null
+        set(value) {
+            okayButtonColor = value
+            cancelButtonColor = value
+            field = value
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_modal_edit_text)
+        initModalElements()
+    }
+
+    override fun initModalElements() {
+        bodyContainer = rll_ModalEditTextContainer
+        closeButton = btn_ModalEditTextCancel
+        super.initModalElements()
         initElements()
     }
 
     private fun initElements() {
-        vgBodyContainer = rll_ModalEditTextContainer
         txtTitle = sct_ModalEditTextTitle
         sctEditText = sct_ModalEditText
         btnSubmit = btn_ModalEditTextOk
@@ -111,30 +135,19 @@ class SabianEditTextModal(
             }
         }
 
-//        sctEditText?.addTextChangedListener(object : TextWatcher {
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                capturedText = s?.toString() ?: ""
-//            }
-//
-//            override fun afterTextChanged(s: Editable?) {}
-//
-//        })
-    }
 
-    override fun show() {
-        val lp = WindowManager.LayoutParams()
-        lp.copyFrom(window?.attributes)
-        lp.width = WindowManager.LayoutParams.MATCH_PARENT
-        lp.height = WindowManager.LayoutParams.MATCH_PARENT
-        super.show()
-        window?.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM)
-        window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
-        window?.attributes = lp
-        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        if (animate) {
-            val anim = AnimationUtils.loadAnimation(context, R.anim.modal_popup_show)
-            vgBodyContainer?.startAnimation(anim)
+
+        okayButtonColor?.let {
+            okayButtonColor = it
+        }
+
+        cancelButtonColor?.let {
+            cancelButtonColor = it
+        }
+
+        buttonColors?.let {
+           buttonColors = it
         }
     }
+
 }
